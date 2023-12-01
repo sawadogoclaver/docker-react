c7908c933f0afa2c7750a506defe5a9805b65b5f
sawadogoclaver <sawadogoclaver@gmail.com>
2023-12-01 11:40:10 +0000

    Expose port and edit App.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deploiement sur AWS beanstalk
         </p>
         <a
           className="App-link"
